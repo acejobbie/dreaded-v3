195b2f4b38e8ec10df87782a591030ffc6b3ec05
Ace <121753552+acejobbie@users.noreply.github.com>
2025-02-13 08:04:57 +0300

    𒋨🏴‍☠️⃝𝘼𝘾𝙀☠️𝐂𝐋͢𝐢𝚵𝐍͢𝙏⃝𒋨🉑

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const session = process.env.SESSION || '';
 const mongoURI = process.env.MONGO_URI || '';
 const mycode = process.env.CODE || '254';
-const botname = process.env.BOTNAME || 'DREADED';
+const botname = process.env.BOTNAME || '𒋨🏴‍☠️⃝𝘼𝘾𝙀☠️𝘽𝙊𝙏⃝𒋨🉑';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
 
